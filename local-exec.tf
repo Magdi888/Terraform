182aba473d775214075de5251f577e944a6ff1ad
Ahmed Magdi <a.magdi888@gmail.com>
2022-07-27 20:19:27 +0200

    add Bastion/Jumphost configuration

--- local-exec.tf
+++ local-exec.tf
@@ -9,7 +9,21 @@ EOF
 
 resource "local_file" "private_key" {
     filename = "./key.pem"
+    file_permission = 0400
     content = <<EOF
 ${tls_private_key.mykey.private_key_pem}
 EOF
 }
+
+
+resource "local_file" "private_key" {
+    filename = "./config"
+    depends_on = [local_file.private_key]
+    content = <<EOF
+Host ${aws_instance.application.private_ip}
+    proxycommand ssh -o StrictHostKeyChecking=no -W %h:%p -i ./key.pem ubuntu@${aws_instance.bastion.public_ip}
+    StrictHostKeyChecking no
+EOF
+}
+
+
