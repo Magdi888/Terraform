f6173b151f6bae3f796dad08d3a51e322fc6b1d9
Ahmed Magdi <a.magdi888@gmail.com>
2022-07-27 16:43:48 +0200

    edit outputs

--- output.tf
+++ output.tf
@@ -5,3 +5,7 @@ output "bastion_ip" {
 output "application_ip" {
     value = aws_instance.application.private_ip
 }
+
+output "private_key" {
+    value = aws_secretsmanager_secret_version.e.secret_string
+}
