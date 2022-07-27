199e7a5c573328f7788a32277a4236fd1a8ec80a
Ahmed Magdi <a.magdi888@gmail.com>
2022-07-27 16:32:53 +0200

    edit local-exec

--- loca-exec.tf
+++
@@ -1,6 +0,0 @@
-resource "null_resource" "example1" {
-  provisioner "local-exec" {
-    command = "echo ${application_ip} >> ./hosts"
-  }
-}
-
---
+++ local-exec.tf
@@ -0,0 +1,7 @@
+resource "local_file" "inventory" {
+    filename = "./hosts"
+    content = <<EOF
+    [webserver]
+    ${aws_instance.application.private_ip}
+    EOF
+}
