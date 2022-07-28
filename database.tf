d0c901b0e74b138b45bd036b97d529fad5fc2756
Ahmed Magdi <a.magdi888@gmail.com>
2022-07-28 18:54:45 +0200

    edit elastic-cache

--- database.tf
+++ database.tf
@@ -27,7 +27,7 @@ resource "aws_elasticache_cluster" "elasticache_cluster" {
   engine               = "redis"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
+  parameter_group_name = "default.redis3.2.cluster.on"
   engine_version       = "3.2.10"
   port                 = 6379
   subnet_group_name = aws_elasticache_subnet_group.redis_subnets.name
