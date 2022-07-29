ea1549e0757b7d5d260ce51e95910adb2d79c5a9
Ahmed Magdi <a.magdi888@gmail.com>
2022-07-30 01:03:22 +0200

    edit lb

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -21,7 +21,7 @@ resource "aws_lb_listener" "front_end" {
 
 resource "aws_lb_target_group" "target_group" {
   name     = "lb-target-group"
-  port     = 3000
+  port     = 80
   protocol = "HTTP"
   vpc_id   = module.network.vpc_id
 }
@@ -29,6 +29,6 @@ resource "aws_lb_target_group" "target_group" {
 
 resource "aws_lb_target_group_attachment" "aws_lb_target_group_attachment" {
   target_group_arn = aws_lb_target_group.target_group.arn
-  target_id        = aws_instance.bastion.id
+  target_id        = aws_instance.application.id
   port             = 3000
 }
