10d52016c60bb45393f4b9f830a95fd06988957a
Ahmed Magdi <a.magdi888@gmail.com>
2022-07-30 03:28:07 +0200

    edit redis

--- local-exec.tf
+++ local-exec.tf
@@ -45,7 +45,7 @@ RDS_HOSTNAME=${aws_db_instance.myrds.address}
 RDS_USERNAME=${aws_db_instance.myrds.username}
 RDS_PASSWORD=${aws_db_instance.myrds.password}
 RDS_PORT=${aws_db_instance.myrds.port}
-REDIS_HOSTNAME=${aws_elasticache_replication_group.elasticache_cluster.configuration_endpoint_address}
+REDIS_HOSTNAME=${aws_elasticache_replication_group.elasticache_cluster.primary_endpoint_address}
 REDIS_PORT=${aws_elasticache_replication_group.elasticache_cluster.port}
 EOF
 }
